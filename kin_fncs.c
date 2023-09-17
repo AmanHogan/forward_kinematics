32f9e30ac2dc99d83aabf2c5f3e2747a01e214cc
AmanHogan <ahoganbailey@gmail.com>
2023-09-17 13:54:39 -0500

    Added Jacobian Matrix and Inverse Matrix

--- kin_fncs.c
+++ kin_fncs.c
@@ -5,84 +5,19 @@
 #define M_PI 3.1415927
 #endif
 
-// Counter-clockwise Rotation Matricies: 
-// RX = {{1, 0, 0}, {0, cos(theta[]), -sin(theta[])}, {0, sin(theta[]), cos(theta[])}}
-// RY = {{cos(theta[]), 0, sin(theta[])}, {0, 1, 0}, {-sin(theta[]), 0, cos(theta[])}}
-// RZ = {{cos(theta[]), -sin(theta[]), 0}, {sin(theta[]), cos(theta[]), 0}, {0, 0, 1}}
-
 /**
- * @param x*
- * @param theta[6]  
-**/
-void inv_kin(double *x, double theta[6]) { }
-
-/**
- * Combines R and D to get T
- * @param R[3][3] Rotation Matrix
- * @param D[3] Displacement Matrix
- * @param T[4][4] Homogenous Transform Matrix 
- * @return None
-**/
-void transform(double R[3][3], double D[3], double T[4][4])
-{
-    int i = 0;
-    int j = 0;
-    for (i = 0; i < 3; i++)
-    {
-        for(j = 0; j < 3; j++)
-        {
-            T[i][j] = R[i][j];
-        }
-    }
-
-    // Set Distance Vector in Transform matrix
-    T[0][3] = D[0];
-    T[1][3] = D[1]; 
-    T[2][3] = D[2]; 
-    T[3][3] = 1; 
-
-    // Extend Matrix
-    T[3][0] = 0; 
-    T[3][1] = 0; 
-    T[3][2] = 0; 
-}
-
-/**
- *  Multiplys two matricies to get resultant matrix
- * @param  A[4][4] Left Transform Matrix
- * @param  B[4][4] Right Transform Matrix
- * @param  C[4][4] Resultant Transform Matrix 
- * @return None
-**/
-void multiplyMatrices(double A[4][4], double B[4][4], double C[4][4]) 
-{
-    int i, j, k;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            C[i][j] = 0;
-            for (k = 0; k < 4; k++) {
-                C[i][j] += (double) A[i][k] * (double) B[k][j];
-            }
-        }
-    }
-}
-
-/**
- * Prints Matrix
- * @param T[4][4] Homogenous Transform Matrix
+ * Prints Jacobian Matrix
+ * @param J[3][4] Jacobian Matrix
  * @returns None
 **/
-void printMatrix(double T[4][4]) 
-{
-    for (int i = 0; i < 4; i++) 
-    {
-        for (int j = 0; j < 4; j++) 
-        {
-            printf("%.4lf\t", T[i][j]);
-        }
-        printf("\n");
-    }
-}
+void print_jacobian(double J[3][4]);
+
+/**
+ * Finds Jacobian Matrix
+ * @param theta[6] Contains five joint angles of the robot arm
+ * @returns None   
+**/
+void jacobian(double theta[6]);
 
 /**
  * Performs Forward Kinematics given joint angles
@@ -92,79 +27,124 @@ void printMatrix(double T[4][4])
 **/
 void fwd_kin(double theta[6], double x[3]) 
 {
-    // Distances joints 
-    double D1 = -0.040;
-    double D2 = 0.040;
-    double D3 = -0.040;
-    double D4 = -0.040;
+    // Distances between joints: D[1]: -.04 | D[2]: .040 | D[3]: -.040 | D[4]: .040 |
+    double D[6] = {0, -0.040, 0.040, -0.040, -0.040, 0 };
 
-    // Lengths Between Joints
-    double L0 = 0.250;
-    double L1 = .200;
-    double L2 = .200;
-    double L3 = .150;
+   // Lengths between joints: L[0]: -.04 | L[1]: .040 | L[2]: -.040 | L[3]: .040 |
+    double L[4] = {0.250, .200, .200, .150};
 
-    ///////////////////////////////////////////////////////////////////
-    //  Derivation: Transformation matrix from Tool frame to Base frame
-    //  TTB = (T0B) (T10) (T21) (T32) (TT3)
-    //  T0B = RZ(0) DZ(LO)
-    //  T10 = RY(1) DX(L1) DY(D1)
-    //  T21 = RY(2) DX(L2) DY(D2)
-    //  T32 = RY(3) DZ(D4) DY(D3)
-    //  TT3 = RX(4) DX(L3)
-    ///////////////////////////////////////////////////////////////////
-
-    // Frame from 0 to base
-    // T0B = RZ(0) DZ(LO)
-    double T0B[4][4];
-    double R0B[3][3] = {{ cos(theta[0]), -sin(theta[0]), 0 }, {sin(theta[0]), cos(theta[0]), 0 }, {0, 0, (double) 1}};
-    double D0B[3] =  {0, 0, L0};
-    transform(R0B,D0B,T0B);
+    // Calculated X Position
+    double X = cos(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) - (D[3]*sin(theta[0]));
     
-    // Frame from 1 to 0
-    // T10 = RY(1) DX(L1) DY(D1)
-    double T10[4][4];
-    double R10[3][3] = {{cos(theta[1]), 0, sin(theta[1])}, {0, (double) 1, 0}, {-sin(theta[1]), 0, cos(theta[1])}};
-    double D10[3] =  {cos(theta[1]) * L1 , D1, sin(theta[1]) * L1};
-    transform(R10,D10,T10);
+    // Calculated Y Position
+    double Y = sin(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) + (D[3]*cos(theta[0]));
+    
+    // Calculated Z Position
+    double Z = (L[0]) - (L[1]*sin(theta[1])) - (L[2]*sin(theta[1]+theta[2])) +  (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3]));
 
-    // Frame from 2 to 1
-    // T21 = RY(2) DX(L2) DY(D2)
-    double T21[4][4];
-    double R21[3][3] = {{cos(theta[2]), 0, sin(theta[2])}, {0, (double) 1, 0}, {-sin(theta[2]), 0, cos(theta[2])}};
-    double D21[3] =  {L2, D2, sin(theta[2]) * L2};
-    transform(R21,D21,T21);
+    // Set Position Vector
+    x[0] = X;
+    x[1] = Y;
+    x[2] = Z;
 
-    // Frame from 3 to 2
-    // T32 = RY(3) DZ(D4) DY(D3)
-    double T32[4][4];
-    double R32[3][3] = {{cos(theta[3]), 0, sin(theta[3])}, {0, (double) 1, 0}, {-sin(theta[3]), 0, cos(theta[3])}};
-    double D32[3] =  {sin(theta[3]) * D3, D3, (cos(theta[3]) * D4)};
-    transform(R32,D32,T32);
+    // Additionally calculate Jacobian Matrix and print it
+    jacobian(theta);
+}
 
-    // Frame from T to 3
-    // TT3 = RX(4) DX(L3)
-    double TT3[4][4];
-    double RT3[3][3] = {{(double) 1, 0, 0}, {0, cos(theta[4]), -sin(theta[4])}, {0, sin(theta[4]), cos(theta[4])}};
-    double DT3[3] =  {L3, 0, 0};
-    transform(RT3,DT3,TT3);
+/**
+ * @param x[3]
+ * @param theta[6]  
+**/
+void inv_kin(double x[3], double theta[6]) 
+{
+   // Distances between joints: D[1]: -.04 | D[2]: .040 | D[3]: -.040 | D[4]: .040 |
+    double D[6] = {0, -0.040, 0.040, -0.040, -0.040, 0 };
 
-    // Outward Inward Derivation
-    // TTB = (T0B) (T10) (T21) (T32) (TT3)
-    double T1B[4][4];
-    double T2B[4][4];
-    double T3B[4][4];
-    double TTB[4][4];
+   // Lengths between joints: L[0]: -.04 | L[1]: .040 | L[2]: -.040 | L[3]: .040 |
+    double L[4] = {0.250, .200, .200, .150};
 
-    // Multiply
-    multiplyMatrices(T0B, T10, T1B);
-    multiplyMatrices(T1B, T21, T2B);
-    multiplyMatrices(T2B, T32, T3B);
-    multiplyMatrices(T3B, TT3, TTB);
+    // Find constant offset of theta[0] | Theta[0] = arcsin((d3)/sqrt(x^2 + y^2))
+    double offset = asin((D[3])/(sqrt(pow((x[0]),2) + pow((x[1]), 2))));
+    
+    // Find theta[0] | Theta[0] = invertan(y/x) - offset
+    theta[0] = atan(x[1]/x[0]) - offset;
 
-    // Set position to last column in Transformation Matrix
-    x[0] = TTB[0][3];
-    x[1] = TTB[1][3];
-    x[2] = TTB[2][3];
+    // Move base frame relative to wrist
+    double x_1[3];
+    x_1[0] = x[0] + D[2] * sin(theta[0]) ;
+    x_1[1] = x[1] - D[2] * cos(theta[0]); 
+    x_1[2] = x[2] - L[0];
 
+    // Move tool frame to wrist
+    double x_2[3];
+    x_2[0] = x_1[0] - D[4] * cos(theta[0]);
+    x_2[1] = x_1[1] - D[4] * sin(theta[0]);
+    x_2[2] = x_1[2] + L[3];
+
+    // Derive 3DOF Triangle
+    double x_3[3];
+    x_3[0] = sqrt((pow(x_2[0], 2)) + (pow(x_2[1],2)));
+    x_3[1] = -x_2[2];
+
+    // Derive Theta[2]
+    double r = sqrt((pow(x_3[0], 2)) + (pow(x_3[1],2)));
+    double beta = atan(x_3[1]/x_3[0]); 
+    theta[2] = acos((pow(r,2) - pow(L[1],2) - pow(L[2], 2)) / (2 * L[1] * L[2]));
+
+    // Derive Theta[1]
+    double psi = acos((pow(r,2) + pow(L[1],2) - pow(L[2], 2)) / (2 * L[1] * r));
+    theta[1] = beta + psi;
+
+    // Derive Theta[3]
+    theta[3] = (M_PI/2) - theta[1] - theta[2];
+}
+
+void jacobian(double theta[6])
+{
+    // Distances between joints: D[1]: -.04 | D[2]: .040 | D[3]: -.040 | D[4]: .040 |
+    double D[6] = {0, -0.040, 0.040, -0.040, -0.040, 0 };
+
+    // Lengths between joints: L[0]: -.04 | L[1]: .040 | L[2]: -.040 | L[3]: .040 |
+    double L[4] = {0.250, .200, .200, .150};
+
+    double X = cos(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) - (D[3]*sin(theta[0]));
+    double Y = sin(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) + (D[3]*cos(theta[0]));
+    double Z = (L[0]) - (L[1]*sin(theta[1])) - (L[2]*sin(theta[1]+theta[2])) +  (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3]));
+
+    // Partial Derivatives for x
+    double x_0 = -sin(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) - (D[3]*cos(theta[0]));
+    double x_1 = cos(theta[0]) * (-(L[1]*sin(theta[1])) - (L[2]*sin(theta[1]+theta[2])) + (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+    double x_2 = cos(theta[0]) * (-(L[2]*sin(theta[1]+theta[2])) + (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+    double x_3 = cos(theta[0]) * ((D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+
+    // Partial Derivatives for y
+    double y_0 = cos(theta[0]) * ((L[1]*cos(theta[1])) + (L[2]*cos(theta[1]+theta[2])) + (D[4]*sin(theta[1]+theta[2]+theta[3])) + (L[3]*cos(theta[1]+theta[2]+theta[3]))) - (D[3]*sin(theta[0]));
+    double y_1 = sin(theta[0]) * (-(L[1]*sin(theta[1])) - (L[2]*sin(theta[1]+theta[2])) + (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+    double y_2 = sin(theta[0]) * (-(L[2]*sin(theta[1]+theta[2])) + (D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+    double y_3 = sin(theta[0]) * ((D[4]*cos(theta[1]+theta[2]+theta[3])) - (L[3]*sin(theta[1]+theta[2]+theta[3])));
+
+    // Partial Derivatives for z
+    double z_0 = 0;
+    double z_1 = - (L[1]*cos(theta[1])) - (L[2]*cos(theta[1]+theta[2])) - (D[4]*sin(theta[1]+theta[2]+theta[3])) - (L[3]*cos(theta[1]+theta[2]+theta[3]));
+    double z_2 = - (L[2]*cos(theta[1]+theta[2])) - (D[4]*sin(theta[1]+theta[2]+theta[3])) - (L[3]*cos(theta[1]+theta[2]+theta[3]));
+    double z_3 = - (D[4]*sin(theta[1]+theta[2]+theta[3])) - (L[3]*cos(theta[1]+theta[2]+theta[3]));
+
+    double jacobian_matrix[3][4] = {{x_0, x_1, x_2, x_3},
+                                    {y_0, y_1, y_2, y_3},
+                                    {z_0, z_1, z_2, z_3}};
+    print_jacobian(jacobian_matrix);
+}
+
+void print_jacobian(double J[3][4]) 
+{
+    printf("Jacobian Matrix: \n");
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 4; j++) 
+        {
+            printf("%.4lf\t", J[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
